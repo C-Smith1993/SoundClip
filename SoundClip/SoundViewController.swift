7499d5f9706047eebe53ff0a39985e55aacbb2ea
C-Smith1993 <chrissmithreds93@hotmail.com>
2017-03-06 21:04:54 +0000

    This microphone app is fully functional. It works well with Core Data.

--- SoundClip/SoundViewController.swift
+++ SoundClip/SoundViewController.swift
@@ -14,13 +14,19 @@ class SoundViewController: UIViewController {
     
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
     
-    var audioRecorder : AVAudioRecorder? = nil
+    var audioRecorder : AVAudioRecorder?
+    var audioPlayer : AVAudioPlayer?
+    var audioURL : URL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupRecorder()
+        playButton.isEnabled = false
+        addButton.isEnabled = false
     }
     
     
@@ -35,13 +41,23 @@ class SoundViewController: UIViewController {
         
         // Create URL for the audio file
         
+        let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let pathComponents = [basePath, "audio.m4a"]
+        audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+            
         
         // Create settings for the audio recorder
         
-        
+            var settings : [String:AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
+            settings[AVSampleRateKey] = 44100.0 as AnyObject?
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject?
+            
+            
         // Create AudioRecroder object
         
-        audioRecorder = AVAudioRecorder(url: <#T##URL#>, settings: <#T##[String : Any]#>)
+        audioRecorder = try AVAudioRecorder(url: audioURL!, settings: settings)
+        audioRecorder!.prepareToRecord()
         } catch let error as NSError{
             print(error)
             
@@ -49,23 +65,66 @@ class SoundViewController: UIViewController {
     }
     
     
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func recordTapped(_ sender: Any) {
+
+        if audioRecorder!.isRecording {
+            // Stop the recording
+            audioRecorder?.stop()
+            
+            // Change button title to Record
+            recordButton.setTitle("Record", for: .normal)
+            
+            playButton.isEnabled = true
+            addButton.isEnabled = true
+            
+        } else {
+            // Start the recording
+            audioRecorder?.record()
+            
+            
+            // Change button title to stop
+            recordButton.setTitle("Stop", for: .normal)
+        }
     }
     
     @IBAction func playTapped(_ sender: Any) {
+        do {
+        try audioPlayer = AVAudioPlayer(contentsOf: audioURL!)
+            audioPlayer!.play()
+        } catch {}
     }
     
     
     @IBAction func addTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let sound = Sound(context: context)
+        
+        sound.name = nameTextField.text
+        sound.audio = NSData(contentsOf: audioURL!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
     }
-    
-    
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
